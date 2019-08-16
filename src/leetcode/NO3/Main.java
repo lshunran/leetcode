3b7aa2e6dcb372d217dc2e7591f81b29efea59d7
ashun <shunran.li@guanshantech.com>
2019-08-16 11:08:00 +0800

    finish NO3

--- src/leetcode/NO3/Main.java
+++ src/leetcode/NO3/Main.java
@@ -1,35 +1,54 @@
 package leetcode.NO3;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static int findLongest(String s){
-        int result = 0;
+        int result = 0, n = s.length();
         HashMap map = new HashMap();
-        for (int i = 0; i < s.length(); i++) {
-            char a = s.charAt(i);
-            if(!map.containsKey(a)){
-                map.put(a, i);
-            }else{
-                int length = map.size();
-                if(length > result){
-                    result = length;
-                }
-                map = new HashMap();
-                map.put(a, i);
+        for (int i = 0, j = 0; j < n; ) {
+            char a = s.charAt(j);
 
+            if(!map.containsKey(a)){
+
+                map.put(a, j++);
+                result = Math.max(result, j - i);
+
+            }else{
+                i = Math.max(((int)map.get(a) + 1), i);
+                map.remove(a);
             }
         }
-        int length = map.size();
-        if(length > result){
-            result = length;
-        }
+
         return result;
     }
 
+
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+
     public static void main(String[] args) {
-        int n = Main.findLongest("dvdf");
+        int n = Main.findLongest("tmmzuxt");
+        int n1 = Main.lengthOfLongestSubstring("tmmzuxt");
+
         return;
     }
 }
