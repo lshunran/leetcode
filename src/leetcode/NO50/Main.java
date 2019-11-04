d5bbfef3fa7176abb734f968c6a10cdebd89956f
ashun <lshunran@163.com>
2019-11-04 18:16:57 +0800

    50

---
+++ src/leetcode/NO50/Main.java
@@ -0,0 +1,12 @@
+package leetcode.NO50;
+
+public class Main {
+    public static double myPow(double x, int n) {
+        if (n == 0) { return 1; }
+        if (n == 1) { return x; }
+        if (n == -1) { return 1 / x; }
+        double half = myPow(x, n / 2);
+        double rest = myPow(x, n % 2);
+        return rest * half * half;
+    }
+}
